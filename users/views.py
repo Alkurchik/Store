0978e99b368525b93eb19662b968b0f72cd4dc2e
kurchik.sasha@gmail.com <>
2022-12-13 10:21:19 +0200

    add users module

---
+++ users/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from users.models import User
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from users.forms import UserLoginForm
+from django.contrib import auth
+
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+
+    else:
+        form = UserLoginForm()
+    context = {'form': UserLoginForm()}
+    return render(request, 'users/login.html', context)
+
+
+def registration(request):
+    return render(request, 'users/registration.html')
