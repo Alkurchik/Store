23608bffa36d7e8c3a4de638ec6a2ea9622cef46
kurchik.sasha@gmail.com <>
2022-12-15 23:06:09 +0200

    New module: basket > add product, view basket in profile page

--- products/models.py
+++ products/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from users.models import User
 # Create your models here.
 
 
@@ -22,3 +22,12 @@ class Product(models.Model):
     def __str__(self):
         return f'Продукт: {self.name} | Категория: {self.category}'
 
+
+class Basket(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(default=0)
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Корзина для {self.user.username} | Продукт: {self.product.name}'
--- products/views.py
+++ products/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from products.models import ProductCategory, Product
-
+from products.models import ProductCategory, Product, Basket
+from users.models import User
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+
 
 def index(request):
     context = {
@@ -18,3 +20,17 @@ def products(request):
         'categories': ProductCategory.objects.all(),
     }
     return render(request, 'products/products.html', context)
+
+
+def basket_add(request, product_id):
+    product = Product.objects.get(id=product_id)
+    baskets = Basket.objects.filter(user=request.user, product=product)
+
+    if not baskets.exists():
+        Basket.objects.create(user=request.user, product=product, quantity=1)
+    else:
+        basket = baskets.first()
+        basket.quantity += 1
+        basket.save()
+
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
